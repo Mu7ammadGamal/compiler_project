82520686a935942cafc3761fc88cf24493546f59
Mu7ammadGamal <mg1297881@gmail.com>
2022-04-27 06:24:29 +0200

    Code Base Updated

---
+++ src/temp.java
@@ -0,0 +1,18 @@
+public class temp {
+    public static void main(String[] args) {
+
+        System.out.println("Enter two numbers");
+        int first = 10;
+        int second = 20;
+        if(first == 10){
+            first = 20;
+        }
+        else {
+            first = 40;
+        }
+        // add two numbers
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+    }
+}
+
